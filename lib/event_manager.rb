595c48f71f66b63ee0cf001c05a16061d71e7577
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-07 15:11:10 -0600

    complete iteration 3: sunlight API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'sunlight/congress'
 puts "EventManager initialized."
 
 #Iteration 0: loading a file
@@ -11,13 +12,11 @@ puts "EventManager initialized."
   #   puts line
   # end
   #
-  # lines = File.readlines "event_attendees.csv"
   # lines.each do |line|
   #   columns = line.split(",")
   #   p columns
   # end
   #
-  # lines = File.readlines "event_attendees.csv"
   # lines.each do |line|
   #   columns = line.split(",")
   #   name = columns[2]
@@ -57,7 +56,7 @@ puts "EventManager initialized."
   #
   #   puts "#{name} #{zipcode}"
   # end
-  # 
+  #
   # def clean_zipcode(zipcode)
   #   zipcode.to_s.rjust(5,"0")[0..4]
   # end
@@ -68,4 +67,60 @@ puts "EventManager initialized."
   #   zipcode = clean_zipcode(row[:zipcode])
   #
   #   puts "#{name} #{zipcode}"
+  # end
+
+#Iteration 3: Sunlight API
+
+  # Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+  #
+  # def clean_zipcode(zipcode)
+  #   zipcode.to_s.rjust(5,"0")[0..4]
+  # end
+  #
+  # contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+  #
+  # contents.each do |row|
+  #   name = row[:first_name]
+  #
+  #   zipcode = clean_zipcode(row[:zipcode])
+  #
+  #   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  #
+  #   legislator_names = legislators.collect do |legislator|
+  #     "#{legislator.first_name} #{legislator.last_name}"
+  #   end
+  #
+  #   legislators_string = legislator_names.join(", ")
+  #
+  #   puts "#{name} #{zipcode} #{legislators_string}"
+  # end
+  #
+  #refactor...
+  #
+  # Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+  #
+  # def clean_zipcode(zipcode)
+  #   zipcode.to_s.rjust(5,"0")[0..4]
+  # end
+  #
+  # def legislators_by_zipcode(zipcode)
+  #   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  #
+  #   legislator_names = legislators.collect do |legislator|
+  #     "#{legislator.first_name} #{legislator.last_name}"
+  #   end
+  #
+  #   legislator_names.join(", ")
+  # end
+  #
+  # contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+  #
+  # contents.each do |row|
+  #   name = row[:first_name]
+  #
+  #   zipcode = clean_zipcode(row[:zipcode])
+  #
+  #   legislators = legislators_by_zipcode(zipcode)
+  #
+  #   puts "#{name} #{zipcode} #{legislators}"
   # end
